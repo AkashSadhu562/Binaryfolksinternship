54fdbe3d9b7628b3520c2aa3bd03d514f63c9375
Akash Sadhu <68912044+AkashSadhu562@users.noreply.github.com>
2020-08-25 00:42:15 +0530

    Add files via upload

--- intr53.php
+++ intr53.php
@@ -1,15 +1,44 @@
-<?php
-   $channel = array('title' => "What's For Dinner",
-      'link' => 'http://menu.example.com/',
-      'description' => 'Choose what to eat tonight.');
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Laravel</title>
+      <link href = "https://fonts.googleapis.com/css?family=Lato:100" rel = "stylesheet" 
+         type = "text/css">
+      
+      <style>
+         html, body {
+            height: 100%;
+         }
+         body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+         }
+         .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+         }
+         .content {
+            text-align: center;
+            display: inline-block;
+         }
+         .title {
+            font-size: 96px;
+         }
+      </style>
+   </head>
    
-   print "<channel>\n";
-   
-   foreach ($channel as $element => $content) {
-      print " <$element>";
-      print htmlentities($content);
-      print "</$element>\n";
-   }
-   
-   print "</channel>";
-?>
+   <body>
+      <div class = "container">
+         
+         <div class = "content">
+            <div class = "title">Laravel 5.1</div>
+         </div>
+			
+      </div>
+   </body>
+</html>
